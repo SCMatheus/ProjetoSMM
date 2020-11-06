4d5f4d781a9718f4f4479242f9ef800388d92e16
SCMatheus <matheusouzacardoso2016@gmail.com>
2020-11-06 17:28:51 -0300

    Adicionados icones e html de preparo

--- BancoDeDados/CriacaoDeTabelas.sql
+++ BancoDeDados/CriacaoDeTabelas.sql
@@ -89,3 +89,38 @@ VALUES ('Arroz Doce',
 		'https://img.itdg.com.br/tdg/images/recipes/000/156/817/277261/277261_original.jpg?mode=crop&width=710&height=400',
 		'https://www.youtube.com/watch?v=ri3AC7Tt2Uk'
 		)
+INSERT INTO Receitas (Nome, Descricao, Tipo, Ingredientes, ModoDePreparo, TempoDePreparo, NumeroDePorcoes, LinkImagem, LinkVideo)
+VALUES ('Bolo de cuca de babana', 
+		'Um bolo que vai te surpreender com todo seu sabor.',
+		5,
+		'6 bananas nanicas;1 xícara de chá de açúcar;1 1/2 xícara de chá de trigo;1 colher de sopa de canela em pó;3 colheres de sopa de manteiga ou margarina em temperatura ambiente',
+		'4 ovos;3 colheres de sopa de manteiga;2 xícaras de chá de farinha de trigo;1 xícara de chá de amido de milho;1 colher de sopa de fermento em pó;3/4 xícara de chá de leite;2 xícaras de chá de açúcar;Manteiga e farinha de trigo para untar e polvilhar',
+		60,
+		12,
+		'https://img.itdg.com.br/tdg/images/recipes/000/064/162/341496/341496_original.jpg?mode=crop&width=710&height=400',
+		'https://www.youtube.com/watch?v=9ksc3tWO0Xw'
+		)
+		
+INSERT INTO Receitas (Nome, Descricao, Tipo, Ingredientes, ModoDePreparo, TempoDePreparo, NumeroDePorcoes, LinkImagem, LinkVideo)
+VALUES ('Salada de bacalhau', 
+		'Uma deliciosa salada de bacalhau muito fácil de fazer.',
+		3,
+		'200 g de bacalhau;2 batatas grandes;1 cenoura;ervilha fresca a gosto;1 pimentão vermelho em rodelas;1 cebola média em rodelas;1 dente de alho pequeno em fatias;cheiro-verde a gosto;15 ovinhos de codorna;azeite a gosto;vinagre a gosto;orégano a gosto;sal a gosto',
+		'Coloque o bacalhau de molho de uma dia para o outro para tirar o sal.;Cozinhe as batatas e as cenouras em rodelas, cozinhe também a ervilha fresca e os ovinhos de codorna.;Retire a água do bacalhau, desfie bem e refogue ligeiramente com uma colher de azeite.;Refogue ligeiramente o pimentão, a cebola e o alho.;Monte numa travessa na seguinte ordem: primeiro a batata, depois a cenoura, o pimentão, o bacalhau, a cebola e o alho.;Coloque a ervilha e decore com os ovinhos partidos ao meio e o cheiro-verde à vontade.;Faça um tempero com o vinagre, sal, orégano e um pouqinho de azeite e despeje uniformemente sobre a salada e deixe descansar.',
+		40,
+		3,
+		'https://img.itdg.com.br/tdg/images/recipes/000/089/189/95998/95998_original.jpg?mode=crop&width=710&height=400',
+		'https://www.youtube.com/results?search_query=salada+de+bacalhau'
+		)
+		
+INSERT INTO Receitas (Nome, Descricao, Tipo, Ingredientes, ModoDePreparo, TempoDePreparo, NumeroDePorcoes, LinkImagem, LinkVideo)
+VALUES ('Arroz Doce', 
+		'Um arroz doce que vai te fazer lembrar da casa da vovó.',
+		2,
+		'1 xícara de arroz;500 ml de água quente para cozinhar o arroz;600 ml de leite quente;1 caixa de leite condensado (1/2 para quem não gosta do arroz muito doce);200 g de creme de leite;canela em pó a gosto',
+		'Cozinhe o arroz em fogo baixo até ficar macio.;Aqueça o leite separadamente em uma panela até ferver.;Quando o arroz estiver cozido, acrescente o leite quente e vá mexendo para não grudar no fundo da panela.;Incorpore o leite condensado e a canela na quantidade de sua preferência e vá mexendo até engrossar.;Desligue o fogo, adicione o creme de leite e misture por mais alguns minutos.;Está pronto para servir.',
+		30,
+		6,
+		'https://img.itdg.com.br/tdg/images/recipes/000/156/817/277261/277261_original.jpg?mode=crop&width=710&height=400',
+		'https://www.youtube.com/watch?v=ri3AC7Tt2Uk'
+		)
