4b027fabe352a7c74ffaf573d3245f3ef8c1464a
SCMatheus <matheusouzacardoso2016@gmail.com>
2020-11-06 00:12:30 -0300

    Criação do repositorio da tabela receitas

---
+++ CozinhaDeVo/CozinhaDeVo/Repositorios/RepositorioBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using CozinhaDeVo.Data;
+using CozinhaDeVo.Repositorios.Interfaces;
+
+namespace CozinhaDeVo.Repositorios
+{
+    public abstract class RepositorioBase<TEntity> : IDisposable,
+       IRepositorioBase<TEntity> where TEntity : class
+    {
+        CozinhaDeVoContext ctx = new CozinhaDeVoContext();
+        public IQueryable<TEntity> GetAll()
+        {
+            return ctx.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return GetAll().Where(predicate).AsQueryable();
+        }
+
+        public TEntity Find(params object[] key)
+        {
+            return ctx.Set<TEntity>().Find(key);
+        }
+
+        public void Atualizar(TEntity obj)
+        {
+            ctx.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void SalvarTodos()
+        {
+            ctx.SaveChanges();
+        }
+
+        public void Adicionar(TEntity obj)
+        {
+            ctx.Set<TEntity>().Add(obj);
+        }
+
+        public void Excluir(Func<TEntity, bool> predicate)
+        {
+            ctx.Set<TEntity>()
+                .Where(predicate).ToList()
+                .ForEach(del => ctx.Set<TEntity>().Remove(del));
+        }
+
+        public void Dispose()
+        {
+            ctx.Dispose();
+        }
+    }
+}
---
+++ CozinhaDeVo/CozinhaDeVo/Repositorios/RepositorioReceita.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CozinhaDeVo.Models;
+using CozinhaDeVo.Repositorios.Interfaces;
+
+namespace CozinhaDeVo.Repositorios
+{
+    public class RepositorioReceita : RepositorioBase<Receita>,IRepositorioReceita
+    {
+    }
+}
